13e8b5ca76952294b66384d4d8d400d65db1c61f
OlgaAlessia <47818476+OlgaAlessia@users.noreply.github.com>
2023-01-08 13:43:30 -0500

    Update main.js

--- wp-includes/js/main.js
+++ wp-includes/js/main.js
@@ -1,15 +1,10 @@
-//lightbox.option({ 'showImageNumberLabel': false });
-
-/*
-lightbox.run('.tz-gallery', { captions: createCaption });
-
-function createCaption(element) {
-    return '<button class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-download-alt"></span></button>';
-} */
 
 jQuery(document).ready(function($) { 
     /*baguetteBox.run('.baguetteBox', { animation: 'fadeIn', noScrollbars: true, captions: createCaption });*/
 
+    var pageGridPicture = $('body').hasClass('page-template-tpl-gridPictures');
+
+    if (pageGridPicture) {
     baguetteBox.run('.baguetteBox', {
         captions: createCaption, // display image captions.
         buttons: 'auto', // arrows navigation
@@ -21,18 +16,9 @@ jQuery(document).ready(function($) {
         preload: 2,
         animation: 'slideIn', // fadeIn or slideIn
         verlayBackgroundColor: 'rgba(0,0,0,.8)'});
+    }
 });
 
 function createCaption(element) {
-    return element.getElementsByTagName('img')[0].alt +'<button class="m-2 btn btn-danger btn-xs" onclick="DownloadImage()">Dowload</button>';
+    return element.getElementsByTagName('img')[0].alt +'<button class="m-2 btn btn-danger btn-xs">Dowload</button>';
 }
-
-    //var caption = element.getElementsByTagName('img')[0].alt;
-    //console.log(caption);
-    //if (caption = "")  {
-    //    return '<button class="m-2 btn btn-danger btn-xs" onclick="DownloadImage()">Dowload</button>';
-
-function DownloadImage() {
-    return this;
-}
-
