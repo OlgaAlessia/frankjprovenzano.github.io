973697b9f37eafc136a03aed3c067fb6a7f85205
OlgaAlessia <47818476+OlgaAlessia@users.noreply.github.com>
2023-01-10 12:39:22 -0500

    Add files via upload

--- resourses/js/main.js
+++ resourses/js/main.js
@@ -1,5 +1,5 @@
-// Create Base64 Object
-var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+var photoName = "";
+var url = "";
 
 jQuery(document).ready(function($) { 
     /*baguetteBox.run('.baguetteBox', { animation: 'fadeIn', noScrollbars: true, captions: createCaption });*/
@@ -8,7 +8,9 @@ jQuery(document).ready(function($) {
 
     if (pageGridPicture) {
         baguetteBox.run('.gallery-wrapper', {
+            fullScreen: false,
             captions: createCaption, // display image captions.
+            noScrollbars: true,
             preload: 4,
             animation: 'fadeIn', // fadeIn or slideIn
         });
@@ -16,34 +18,136 @@ jQuery(document).ready(function($) {
 });
 
 function createCaption(element) {
-    return  element.getElementsByTagName('img')[0].alt +'<button class="m-2 btn" onclick="sendEmail()"><i class="fa fa-download"></i> Download</button>';
+    return  element.getElementsByTagName('img')[0].alt +'<button class="m-2 btn" onclick="sendEmail(\'' + element.id + '\')"><i class="fa fa-download"></i> Download</button>';
 }
 
+//https://frankjprovenzano.com/resourses/images/08.jpg
+
+function sendEmail(e) {
+    photoName = e +'.jpg';
+    url = 'https://frankjprovenzano.com/resourses/images/'+ e +'.jpg';
+    
+    location.href = 'emailForm.html?url='+url;
+}
+
+/*
 function sendEmail() {
-
-imagePath="D:/OLGA/ProgettoFrank/wpContent/images/08.jpg";
-
-// Encode the String
-var encodedString = Base64.encode(imagePath);
-console.log(encodedString); 
 Email.send({
-    Host : "smtp.mailtrap.io",
-    SecureToken : "22ee4c79a8614be2841e325feaad3d65",
-    Username: 'b36c527aaf72bd',
-    Password: '17d3950eed7bef',
-    To : 'quaranta.olga@gmail.com',
-    From : "quaranta.olga@gmail.com",
+    Host: "smtp.gmail.com",
+    Username: 'olga.cercalavoro@gmail.com',
+    Password: '0r1g1n3_Perdut@',
+    To : 'gugli.venturi@gmail.com',
+    From : "olga.cercalavoro@gmail.com",
     Subject : "Test Email",
     Body : "<html><h2>Hello</h2>This is the image you<strong>downloaded</strong>.</br></html>",
 	Attachments : [
 	{
-		name : "31-314651_black-cat-clipart-png-figaro-gatto.png",
-		path : "https://www.pngkit.com/png/full/31-314651_black-cat-clipart-png-figaro-gatto.png"
+		name : photoName,
+		path : url
 	}]
-	}).then(
-  		message => alert(message)
-	);
+	}).then(function (message) {
+        console.log("mail sent successfully");
+        alert("mail sent successfully")
+    });
+
+    .then(
+        message => alert(message)
+  )
 }
+*/
+
+/*
+
+function sendEmail(){
+    console.log("I touch Download");
+    try{
+        var theApp = new ActiveXObject("Outlook.Application");
+        var objNS = theApp.GetNameSpace('MAPI');
+        var theMailItem = theApp.CreateItem(0); // value 0 = MailItem
+        theMailItem.to = ('quaranta.olga@gmail.com');
+        theMailItem.Subject = ('Test Email');
+        theMailItem.Body = ('<html><h2>Hello</h2>This is the image you<strong>downloaded</strong>.</br></html>');
+        theMailItem.Attachments.Add(photoName);
+        theMailItem.display();
+    }
+    catch (err) {
+        alert(err.message);
+    } 
+}
+*/
+
+/*
+function send_email() {
+    var result = "";
+
+    var oMail = new EASendMail.SmtpMail("TryIt");
+
+    // Set sender email address, please change it to yours
+    oMail.from = new EASendMail.MailAddress("olga.cercalavoro@gmail.com");
+
+    // Add recipient email address, please change it to yours
+    oMail.to.add(new EASendMail.MailAddress("quaranta.olga@gmail.com"));
+
+    // Set email subject
+    oMail.subject = "Test email with attachment from JavaScript Frank project";
+
+    // Set HTML body
+    oMail.htmlBody = "<font size=5>This is</font> <font color=red><b>a test</b></font>";
+
+    var oServer = new EASendMail.SmtpServer("smtp.gmail.net");
+
+    // User and password for ESMTP authentication
+    oServer.user = "olga.cercalavoro@gmail.com";
+    oServer.password = "TestPassword";
+
+    // If your SMTP server requires TLS connection on 25 port, please add this line
+    // oServer.connectType = EASendMail.SmtpConnectType.connectSSLAuto;
+
+    // If your SMTP server requires SSL connection on 465 port, please add this line
+    // oServer.port = 465;
+    // oServer.connectType = EASendMail.SmtpConnectType.connectSSLAuto;
+
+    var oSmtp = new EASendMail.SmtpClient();
 
 
+    // get a file path from PicturesLibrary,
+    // to access files in PicturesLibrary, you MUST have "Pictures Library" checked in
+    // your project -> Package.appxmanifest -> Capabilities
+    Windows.Storage.KnownFolders.picturesLibrary.getFileAsync("test.jpg")
+    .then(function (file) {
 
+        //var attfile = file.path;
+        // if you want to add attachment from remote URL instead of local file.
+        var attfile = "file:///D:/OLGA/ProgettoFrank/resourses/images/02.jpg";
+        // var attfile = "http://www.emailarchitect.net/test.jpg";
+
+        return oMail.addAttachmentAsync(attfile);
+    })
+    .then(function (oAttachment) {
+        // you can change the Attachment name by
+        // oAttachment.name = "mytest.jpg";
+        return oSmtp.sendMailAsync(oServer, oMail);
+    })
+    .done(function () {
+
+        result = "Email was sent successfully!";
+        // Display Result by Diaglog box
+        (new Windows.UI.Popups.MessageDialog(result, "Success")).showAsync();
+    },
+
+    // error handle
+    function (e) {
+        // because javascript exception only gives the stack trace messages, but it is not
+        // real description of exception, so we give a property lastErrorMessage for javascript.
+        if (oMail.lastErrorMessage != "") {
+            result = oSmtp.lastErrorMessage;
+        }
+        else if (oSmtp.lastErrorMessage != "") {
+            result = oSmtp.lastErrorMessage;
+        }
+        else {
+            result = e.message;
+        }
+        (new Windows.UI.Popups.MessageDialog(result, "Error Information")).showAsync();
+    });
+}*/
