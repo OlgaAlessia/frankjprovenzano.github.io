922506f90321d9cd06b34bc9ac5a651a7812c3d5
OlgaAlessia <47818476+OlgaAlessia@users.noreply.github.com>
2023-01-14 19:20:31 -0500

    Add files via upload

--- resourses/js/main.js
+++ resourses/js/main.js
@@ -4,48 +4,47 @@ var url = "";
 jQuery(document).ready(function($) { 
     /*baguetteBox.run('.baguetteBox', { animation: 'fadeIn', noScrollbars: true, captions: createCaption });*/
 
-    var pageGridPicture = $('body').hasClass('page-template-tpl-gridPictures');
-
-    if (pageGridPicture) {
-        baguetteBox.run('.gallery-wrapper', {
-            fullScreen: false,
-            captions: createCaption, // display image captions.
-            noScrollbars: true,
-            preload: 4,
-            animation: 'fadeIn', // fadeIn or slideIn
-        });
-    }
+    baguetteBox.run('.gallery-wrapper', {
+        fullScreen: false,
+        captions: createCaption, // display image captions.
+        noScrollbars: true,
+        preload: 4,
+        animation: 'fadeIn', // fadeIn or slideIn
+    });
+    
 });
 
 function createCaption(element) {
     return  element.getElementsByTagName('img')[0].alt +'<button class="m-2 btn" onclick="sendEmail(\'' + element.id + '\')"><i class="fa fa-download"></i> Download</button>';
 }
 
-function sendEmail(e) {
+function sendEmail(photoName) {
 
-    photoName = e +'.jpg';
-    url = 'https://frankjprovenzano.com/resourses/images/'+ e +'.jpg';
+    url = 'https://frankjprovenzano.com/resourses/images/'+ photoName;
 
     let email = prompt("Please enter your email address");
 
     if (email != null || email != "") {
         Email.send({
             Host: "smtp.elasticemail.com",
-            Username: 'olga.cercalavoro@gmail.com',
-            Password: '05E505F6D8464F6EC37C43CBDC30B04134B3',
-            To : email, //quaranta.olga@gmail.com
+            Username: 'picture@frankjprovenzano.com',
+            Password: '706B1CB8BC75565FCDC2D7A9802CD038E6FB',
+            To : email,
             From : "picture@frankjprovenzano.com",
-            Subject : "Test Email",
-            Body : "<html><h2>Hello</h2>This is the image you<strong>downloaded</strong>.</br></html>",
+            Subject : "Photo of Frank",
+            Body : "<html>Thank you for visiting the site. \n Attached you will find the photo you have selected.\n On behalf of the Provenzano's Family, we thank you for keeping Frank's memory alive. 🤟 </html>",
             Attachments : [
             {
                 name : photoName,
                 path : url
             }]
         })
-        .then(
-            message => alert(message)
-        );
+        .then(function (message) {
+            if (message = "OK") { 
+                alert("Email successfully sent. Check an email from picture@frankjprovenzano.com in your SPAM folder as well.");
+            }
+            else { alert(message); }
+        });
     }else
     {
         alert("Email not valid");
