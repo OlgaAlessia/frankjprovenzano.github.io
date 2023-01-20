9a71ae9ebb8406fb2f97dc524323253a0d8ecff2
OlgaAlessia <47818476+OlgaAlessia@users.noreply.github.com>
2023-01-20 01:38:36 +0100

    Add files via upload

--- resourses/js/main.js
+++ resourses/js/main.js
@@ -18,6 +18,44 @@ jQuery(document).ready(function($) {
         preload: 4,
         animation: 'slideIn', // fadeIn or slideIn
     });
+
+
+    // Animation  //
+    var pagePicture = $('body').hasClass('page-template-tpl-home');
+    var pictureFrank = $('div').hasClass('gallery-wrapper');
+    var $animation_elements = $('.showOnScroll');
+    var $window = $(window);
+
+
+    if (pagePicture && pictureFrank)   {
+        $(window).on('scroll resize', check_if_in_view);
+        $(window).trigger('scroll');
+    }
+
+    function check_if_in_view() {
+        var window_height = $window.height();
+        console.log('window_height= '+window_height);
+        var window_top_position = $window.scrollTop();
+        console.log('window_top_position='+window_top_position);
+        var window_bottom_position = (window_top_position + window_height);
+        console.log('window_bottom_position='+window_bottom_position);
+        
+        $.each($animation_elements, function() {
+            
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+        
+            //check to see if this current container is within viewport
+            if ((element_bottom_position > window_top_position + 100) &&
+                (element_top_position < window_bottom_position - 100 )) {
+                $element.addClass('in-view');
+            } else {
+                $element.removeClass('in-view');
+            }
+        });
+    }
     
 });
 
@@ -33,7 +71,7 @@ function sendEmail(photoName) {
     let email = prompt("Please enter your email address");
 
     if (email == "") {
-        alert("The Email is not valid.");
+        alert("The email address is not valid.");
     }
     if (email != null && email != "" ){
         Email.send({
